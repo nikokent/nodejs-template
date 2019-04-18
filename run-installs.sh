5b3e652e4f19a8cc60d932b4beb3d2e91e852385
nkent <nkent@eecs.wsu.edu>
2019-04-18 16:41:11 -0700

    adding brew and apt-get support

--- run-installs.sh
+++ run-installs.sh
@@ -34,7 +34,17 @@ fi
 
 #check for requirements
 if ! [ -x "$(command -v node)" ]; then
-  echo 'Node is not installed. Please visit https://nodejs.org/en/ and install to continue!' >&2
+  if ! [ -x "$(command -v apt-get)" ]; then
+    if ! [ -x "$(command -v brew)" ]; then  
+      echo "${red}Node is not installed. Please visit https://nodejs.org/en/ and install to continue! >&2${reset}"
+    else
+      echo 'Attempting to install node and npm!'
+      brew install node
+      echo "${red}Please try to restart script${reset}"
+    fi
+  else  
+    echo "${yellow}Attempting to install node and npm${reset}"
+    sudo apt-get install nodejs npm
   exit 1
 else
   echo "${green}Node is installed [✓]${reset}"
